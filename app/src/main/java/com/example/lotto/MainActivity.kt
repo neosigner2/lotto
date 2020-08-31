eb411e35bfc7c1e41139dd239c27f2cb843ab4e2
joontaeKim <neosigner@gmail.com>
2020-08-31 15:18:23 +0900

    20200831

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        title = "역전로또"
 
     }
 }
